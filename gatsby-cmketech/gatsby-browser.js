e7a2fceddbcf4eac7fa20e606ed857d8928b1b1c
stephen <smahadeo@nvisia.com>
2021-07-07 21:50:44 -0500

    added memberstack

--- gatsby-cmketech/gatsby-browser.js
+++ gatsby-cmketech/gatsby-browser.js
@@ -4,12 +4,10 @@ import "./src/assets/style.css"
 import "./src/assets/print.css"
 import "./src/assets/style-theme.css"
 
-window.MemberStack.reload();
-
 const memberStackScript = url => {
     const script = document.createElement("script")
     script.src = url
-    script.setAttribute('data-memberstack-id','b3a96bc0b7fb2bfeb080a0ad91819570')
+    script.setAttribute('data-memberstack-id','5fb7aa8ce9cc74cc2896c9ccc813d979')
     script.async = true
     document.body.appendChild(script)
 }
