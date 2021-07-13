de9623b29618e65a18797f92556a332d4c352f0e
stephen <smahadeo@nvisia.com>
2021-07-13 00:04:52 -0500

    updated header

--- gatsby-cmketech/src/components/Menu.js
+++ gatsby-cmketech/src/components/Menu.js
@@ -28,23 +28,38 @@ const Menu = () => {
   if (!wpMenu?.menuItems?.nodes || wpMenu.menuItems.nodes === 0) return null
 
   return (
-    <nav className="header" aria-label="Horizontal" role="navigation">
-      <ul className="primary-menu reset-list-style"> {
-        wpMenu.menuItems.nodes.map((menuItem, i) => {
-          const path = menuItem?.connectedNode?.node?.uri ?? menuItem.url
-          const itemId = "menu-item-" + menuItem.databaseId
-          return (
-            <li id={itemId} key={i + menuItem.url} className={"menu-item menu-item-type-custom menu-item-object-custom menu-item-home " + itemId}>
-            <UniversalLink to={path} activeClassName={"current-menu-item current_page_item"} >
-            {menuItem.label}
-            </UniversalLink>
-            </li>
-          )
-          }
-        )
-      }
+    <nav>
+      <ul className="nav--top-references">
+        <li><a href="/help"><i className="fas fa-question-circle"></i> <span>Help</span></a></li>
+        <li><a href="/recruit-with-us"><i className="fas fa-users"></i> <span>Recruit with us</span></a></li>
+        <li><a href="/login"><i className="fas fa-sign-in-alt"></i> <span>Login</span></a></li>
+      </ul>
+      <ul className="nav--top-main"> 
+        <li><a href="/jobs">Jobs</a></li>
+        <li><a href="/why-milwaukee">Why Milwaukee</a></li>
+        <li><a href="/companies">Companies</a></li>
+        <li><a href="/tech-scene">Tech scene <i class="fas fa-caret-down"></i></a></li>
+        <li><a href="#" className="search"><i class="fas fa-search"></i></a></li>
       </ul>
     </nav>
+
+    // <nav className="header" aria-label="Horizontal" role="navigation">
+    //   <ul className="primary-menu reset-list-style"> {
+    //     wpMenu.menuItems.nodes.map((menuItem, i) => {
+    //       const path = menuItem?.connectedNode?.node?.uri ?? menuItem.url
+    //       const itemId = "menu-item-" + menuItem.databaseId
+    //       return (
+    //         <li id={itemId} key={i + menuItem.url} className={"menu-item menu-item-type-custom menu-item-object-custom menu-item-home " + itemId}>
+    //         <UniversalLink to={path} activeClassName={"current-menu-item current_page_item"} >
+    //         {menuItem.label}
+    //         </UniversalLink>
+    //         </li>
+    //       )
+    //       }
+    //     )
+    //   }
+    //   </ul>
+    // </nav>
   )
 }
 
