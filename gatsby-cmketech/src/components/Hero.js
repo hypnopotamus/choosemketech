389e0358dbff52de7c109fea7908ef387296af48
nvisiawwebster <wwebster@nvisia.com>
2021-07-14 11:16:57 -0500

    start of refactor of hero component. Making a video element, will switch to image at breakpoint

--- gatsby-cmketech/src/components/Hero.js
+++ gatsby-cmketech/src/components/Hero.js
@@ -4,16 +4,12 @@ import '../assets/sass/components/_hero.scss';
 const Hero = (props) => {
     return (
         <section className="hero--media--container">
-            {
-                props.movieMedia ?
-                    <video src="https://f.hubspotusercontent00.net/hubfs/436951/Elite-Home.mp4"
-                        height="50%" autoPlay="1" muted="" loop="" /> :
-                    <img src="https://via.placeholder.com/1280x500" alt="hero image" />
-            }
+            <video src="https://f.hubspotusercontent00.net/hubfs/436951/Elite-Home.mp4"
+                height="50%" autoPlay="1" muted="" loop="" />
             <div className="hero--large--circle" />
             <div className="hero--small--circle" />
             <div className="hero--white--circle" />
-            <div className="hero--copy--container">
+            <div className="hero--copy">
                 <h1>Build your Tech Career <span>In Milwaukee</span></h1>
                 <h3>Talent + Attraction</h3>
             </div>
