3f183d314a9a754080726e2f8483c8cc639bbaf2
stephen <smahadeo@nvisia.com>
2021-07-16 12:59:19 -0500

    fixed issue with navigation not having url

--- gatsby-cmketech/src/components/Menu.js
+++ gatsby-cmketech/src/components/Menu.js
@@ -1,21 +1,21 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql, Link, useStaticQuery } from "gatsby"
 import UniversalLink from "./UniversalLink"
 
 const Menu = () => {
   return (
     <nav>
       <ul className="nav--top-references">
-        <li><a href="/help"><i className="fas fa-question-circle"></i> <span>Help</span></a></li>
-        <li><a href="/recruit-with-us"><i className="fas fa-users"></i> <span>Recruit with us</span></a></li>
-        <li><a href="/login"><i className="fas fa-sign-in-alt"></i> <span>Login</span></a></li>
+        <li><Link to="/help"><i className="fas fa-question-circle"></i> <span>Help</span></Link></li>
+        <li><Link to="/recruit-with-us"><i className="fas fa-users"></i> <span>Recruit with us</span></Link></li>
+        <li><Link to="/login"><i className="fas fa-sign-in-alt"></i> <span>Login</span></Link></li>
       </ul>
       <ul className="nav--top-main"> 
-        <li><a href="/jobs">Jobs</a></li>
-        <li><a href="/why-milwaukee">Why Milwaukee</a></li>
-        <li><a href="/companies">Companies</a></li>
-        <li><a href="/tech-scene">Tech scene <i class="fas fa-caret-down"></i></a></li>
-        <li><a href="#" className="search"><i class="fas fa-search"></i></a></li>
+        <li><Link to="/jobs">Jobs</Link></li>
+        <li><Link to="/why-milwaukee">Why Milwaukee</Link></li>
+        <li><Link to="/companies">Companies</Link></li>
+        <li><Link to="/tech-scene">Tech scene <i class="fas fa-caret-down"></i></Link></li>
+        <li><Link to="#" className="search"><i class="fas fa-search"></i></Link></li>
       </ul>
     </nav>
 
