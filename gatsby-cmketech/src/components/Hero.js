51fb06278107f47bdf349521805ee71e7d78079e
nvisiawwebster <wwebster@nvisia.com>
2021-07-16 14:53:34 -0500

    copy alignment to video

--- gatsby-cmketech/src/components/Hero.js
+++ gatsby-cmketech/src/components/Hero.js
@@ -4,17 +4,16 @@ import { heroVideo, heroVideo2 } from "../assets/assets";
 
 const Hero = ({}) => {
     return (
-        <section className="hero">
+        <section className="hero--banner--container">
             <video autoPlay="true" id="heroVideo" loop="yes" muted="yes" poster="images/hero-placeholder.png">
                 <source src={heroVideo} type="video/mp4" />
                 <source src={heroVideo2} type="video/webm" />
                 <source src="videos/hero.ogv" type="video/ogv" />
             </video>
-            <div className="hero--content">
+            <div className="hero--copy">
                 <h1>Build Your Tech <br /> Career In <span>Milwaukee, WI</span></h1>
-                <p>CAREERS + CONNECTION</p>
+                <h3>CAREERS + CONNECTION</h3>
             </div>
-            <span>test</span>
         </section>
     )
 }
