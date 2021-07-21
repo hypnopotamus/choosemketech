58641a642fed13304d9dfa462aec0dd2289024e0
nvisiawwebster <wwebster@nvisia.com>
2021-07-21 13:13:06 -0500

    begin of mobile first SCSS changes

--- gatsby-cmketech/src/components/Hero.js
+++ gatsby-cmketech/src/components/Hero.js
@@ -4,15 +4,15 @@ import { heroVideo, heroVideo2, heroBannerMobile, heroBannerTablet } from "../as
 
 const Hero = ({}) => {
     return (
-        <section className="hero__banner-container">
-            <video autoPlay="true" id="heroVideo" loop="yes" muted="yes" poster="images/hero-placeholder.png">
+        <section id="hero" className="hero--banner">
+            <video autoPlay={true} id="heroVideo" loop="yes" muted="yes" poster="images/hero-placeholder.png">
                 <source src={heroVideo} type="video/mp4" />
                 <source src={heroVideo2} type="video/webm" />
                 <source src="videos/hero.ogv" type="video/ogv" />
             </video>
             <img src={heroBannerMobile} alt="skyline of Milwaukee" />
-            <img id="hero__tablet" src={heroBannerTablet} alt="views of Milwaukee" />
-            <div className="hero__copy">
+            <img className="hero--banner__tablet" src={heroBannerTablet} alt="views of Milwaukee" />
+            <div className="hero--banner__copy">
                 <h1>Build Your Tech <br /> Career In <span>Milwaukee, WI</span></h1>
                 <h3>CAREERS + CONNECTION</h3>
             </div>
