82585797500f612489e777cf19a2089d12305b72
nvisiawwebster <wwebster@nvisia.com>
2021-07-22 13:08:38 -0500

    Intro copy component mobile styling checkpoint reached

--- gatsby-cmketech/src/components/IntroCopy.js
+++ gatsby-cmketech/src/components/IntroCopy.js
@@ -3,21 +3,14 @@ import { Link } from "gatsby"
 
 const IntroCopy = ({ }) => {
     return (
-<<<<<<< HEAD
         <section className="intro-copy--container">
             <div className="intro-copy">
-                <h2>Choose Milwaukee Tech</h2>
+                <h1>Choose Milwaukee Tech</h1>
                 <h3>Community &amp; Careers –&nbsp;new Website coming soon</h3>
                 <p>The Milwaukee Tech Hub Coalition is brewing up a home-base for tech candidates and top companies to connect, building careers and community along the way. <strong>To stay in the loop about upcoming tech jobs, events, and the MKE tech scene, sign up for updates below.</strong></p>
+                <button className="button--primary">Button Style 1</button>
             </div>
         </section>
-=======
-        <div className="copy copy--intro">
-            <h2>Choose Milwaukee Tech</h2>
-            <h3>Community &amp; Careers –&nbsp;new Website coming soon</h3>
-            <p>The Milwaukee Tech Hub Coalition is brewing up a home-base for tech candidates and top companies to connect, building careers and community along the way. <strong>To stay in the loop about upcoming tech jobs, events, and the MKE tech scene, sign up for updates below.</strong></p>
-        </div>        
->>>>>>> develop
     )
 }
 
