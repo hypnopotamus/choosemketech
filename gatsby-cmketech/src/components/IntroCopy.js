2cd3f31d05366c3a8f0586d2a2ac03497a53d747
nvisiawwebster <wwebster@nvisia.com>
2021-07-22 16:34:24 -0500

    minor refactor scss and jsx

--- gatsby-cmketech/src/components/IntroCopy.js
+++ gatsby-cmketech/src/components/IntroCopy.js
@@ -3,12 +3,12 @@ import { Link } from "gatsby"
 
 const IntroCopy = ({ }) => {
     return (
-            <section className="intro intro-copy">
-                <h1>Choose Milwaukee Tech</h1>
-                <h3>Community &amp; Careers –&nbsp;new Website coming soon</h3>
-                <p>The Milwaukee Tech Hub Coalition is brewing up a home-base for tech candidates and top companies to connect, building careers and community along the way. <strong>To stay in the loop about upcoming tech jobs, events, and the MKE tech scene, sign up for updates below.</strong></p>
-                <button className="button--primary">Button Style 1</button>
-            </section>
+        <section className="intro intro-copy">
+            <h1>Choose Milwaukee Tech</h1>
+            <h3>Community &amp; Careers –&nbsp;new Website coming soon</h3>
+            <p>The Milwaukee Tech Hub Coalition is brewing up a home-base for tech candidates and top companies to connect, building careers and community along the way. <strong>To stay in the loop about upcoming tech jobs, events, and the MKE tech scene, sign up for updates below.</strong></p>
+            <button className="button--primary">Button Style 1</button>
+        </section>
     )
 }
 
