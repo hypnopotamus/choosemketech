a7c4ae397b3e5c95ecac258490e6a19b3d40c822
nvisiawwebster <wwebster@nvisia.com>
2021-07-22 17:13:01 -0500

    initial image grid start

--- gatsby-cmketech/src/assets/assets.js
+++ gatsby-cmketech/src/assets/assets.js
@@ -3,5 +3,16 @@ import heroBannerTablet from './images/tablet_header_still.png';
 import heroVideo from './videos/SliceOfMilwaukee.mp4';
 import heroVideo2 from './videos/SliceOfMilwaukee.webm';
 
+import imageGrid1 from './images/mke-artmuseum.jpg';
+import imageGrid2 from './images/mke-collectivo.jpg';
+import imageGrid3 from './images/mke-estabrook.jpg';
+import imageGrid4 from './images/mke-summerfest.jpg';
+import imageGrid5 from './images/mke-hubbard.jpg';
+import imageGrid6 from './images/mke-indiafest.jpg';
+import imageGrid7 from './images/mke-lakefront.jpg';
 
+// Hero Assets
 export { heroVideo, heroVideo2, heroBannerMobile, heroBannerTablet };
+
+// image Grid
+export { imageGrid1, imageGrid2, imageGrid3, imageGrid4, imageGrid5, imageGrid6, imageGrid7 }
---
+++ gatsby-cmketech/src/components/ImageGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+import { imageGrid1, imageGrid2, imageGrid3, imageGrid4, imageGrid5, imageGrid6, imageGrid7 } from '../assets/assets';
+
+const ImageGrid = ({ }) => {
+    return (
+        <section className="image-grid">
+            
+        </section>
+    )
+}
+
+export default ImageGrid;
