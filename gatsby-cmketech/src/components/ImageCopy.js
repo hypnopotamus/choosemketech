7eaa6c2cceb6d738493c7aa2f14f6c2082b3bbea
Oliver Ramirez-Velazquez <oramirez@nvisia.com>
2021-07-27 10:58:21 -0500

    responsive fixes

--- gatsby-cmketech/src/components/ImageCopy.js
+++ gatsby-cmketech/src/components/ImageCopy.js
@@ -48,6 +48,58 @@ const ImageCopy = ({}) => {
             </div>
           </div>
         </div>
+        <div className="imageCopy__container imageCopy__featured">
+          <div className="row imageCopy__row">
+            <div className="imageCopy__image">
+              <span className="tag tag--gold tag--gold--featured">Featured</span>
+              <img src="https://via.placeholder.com/592x372" />
+            </div>
+            <div className="imageCopy__copy">
+              <h3>SubHeading 1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor.
+              </p>
+              <div className="imageCopy__copy__ctas">
+                <button className="button button--primary">Button Style</button>
+                <a className="link" href="#">
+                  Thisis a link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="imageCopy__container imageCopy__featured">
+          <div className="row imageCopy__row">
+            <div className="imageCopy__image">
+              <span className="tag tag--gold tag--gold--featured">Featured</span>
+              <img src="https://via.placeholder.com/592x372" />
+            </div>
+            <div className="imageCopy__copy">
+              <h3>SubHeading 1</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+                takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                eirmod tempor.
+              </p>
+              <div className="imageCopy__copy__ctas">
+                <button className="button button--primary">Button Style</button>
+                <a className="link" href="#">
+                  Thisis a link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
         
       </Slider>
     </div>
