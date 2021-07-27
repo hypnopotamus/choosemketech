5f36c356ed3895b840514fb4a4d64026102dae7c
nvisiawwebster <wwebster@nvisia.com>
2021-07-27 15:37:21 -0500

    mock data added for loop

--- gatsby-cmketech/src/components/Accordion.js
+++ gatsby-cmketech/src/components/Accordion.js
@@ -1,87 +1,101 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
 
-const Accordion = ({}) => {
-    const [accordion, setAccordion] = useState(false)
+const Accordion = ({ id }) => {
+    const [selectedAction, setSelectedAction] = useState(null)
 
-    const handleAccordionClick = () => {
-        setAccordion(!accordion)
+    const MOCK_DATA = [
+        {
+            id: 1,
+            header: `Lorem Ipsum 2`,
+            text: `Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Ut id purus at felis suscipit consequat id nec
+                    magna. Vestibulum ante ipsum primis in faucibus orci
+                    luctus et ultrices posuere cubilia curae; Nam purus
+                    odio, tempor sed justo non, tincidunt suscipit leo.
+                    Suspendisse et nibh nec neque iaculis molestie
+                    lacinia id nisl. Nullam nisl metus, semper ac erat
+                    ut, suscipit ullamcorper odio. Nunc et metus ut odio
+                    posuere ultricies in id erat. Quisque luctus posuere
+                    est ut facilisis. Morbi auctor, justo blandit
+                    vehicula fringilla, sem erat commodo nulla, at
+                    scelerisque tellus turpis eget risus. Donec et nulla
+                    in elit aliquet molestie. Suspendisse varius finibus
+                    aliquam. Donec laoreet interdum elit et malesuada.
+                    Nullam pharetra felis vitae lorem pulvinar, rutrum
+                    viverra urna aliquet.`,
+        },
+        {
+            id: 2,
+            header: `Lorem Ipsum 2`,
+            text: ` Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Ut id purus at felis suscipit consequat id nec
+                    magna. Vestibulum ante ipsum primis in faucibus orci
+                    luctus et ultrices posuere cubilia curae; Nam purus
+                    odio, tempor sed justo non, tincidunt suscipit leo.
+                    Suspendisse et nibh nec neque iaculis molestie
+                    lacinia id nisl. Nullam nisl metus, semper ac erat
+                    ut, suscipit ullamcorper odio. Nunc et metus ut odio
+                    posuere ultricies in id erat. Quisque luctus posuere
+                    est ut facilisis. Morbi auctor, justo blandit
+                    vehicula fringilla, sem erat commodo nulla, at
+                    scelerisque tellus turpis eget risus. Donec et nulla
+                    in elit aliquet molestie. Suspendisse varius finibus
+                    aliquam. Donec laoreet interdum elit et malesuada.
+                    Nullam pharetra felis vitae lorem pulvinar, rutrum
+                    viverra urna aliquet.`,
+        },
+    ]
+
+    const handleAccordionClick = (i) => {
+        if (selectedAction == i) {
+            return setSelectedAction(null)
+        }
+        setSelectedAction(i)
     }
 
     return (
         <>
-        <div className={accordion ? `accordion--active` : `accordion`}>
-            <section className={accordion ? `accordion--header__active` : `accordion--header`}>
-                {accordion ? (
-                    <h5>Expanded Accordion Heading</h5>
-                ) : (
-                    <h5>Closed Accordion Heading</h5>
-                )}
-                {accordion ? (
-                    <i class="fas fa-minus" onClick={handleAccordionClick}></i>
-                ) : (
-                    <i class="fas fa-plus" onClick={handleAccordionClick}></i>
-                )}
-            </section>
-            {accordion ? (
-                <section className="accordion--content">
-                    <div className="accordion--content__subheader"></div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Ut id purus at felis suscipit consequat id nec magna.
-                        Vestibulum ante ipsum primis in faucibus orci luctus et
-                        ultrices posuere cubilia curae; Nam purus odio, tempor
-                        sed justo non, tincidunt suscipit leo. Suspendisse et
-                        nibh nec neque iaculis molestie lacinia id nisl. Nullam
-                        nisl metus, semper ac erat ut, suscipit ullamcorper
-                        odio. Nunc et metus ut odio posuere ultricies in id
-                        erat. Quisque luctus posuere est ut facilisis. Morbi
-                        auctor, justo blandit vehicula fringilla, sem erat
-                        commodo nulla, at scelerisque tellus turpis eget risus.
-                        Donec et nulla in elit aliquet molestie. Suspendisse
-                        varius finibus aliquam. Donec laoreet interdum elit et
-                        malesuada. Nullam pharetra felis vitae lorem pulvinar,
-                        rutrum viverra urna aliquet.
-                    </p>
-                </section>
-            ) : null}
-        </div>
-        <div className={accordion ? `accordion--active` : `accordion`}>
-            <section className={accordion ? `accordion--header__active` : `accordion--header`}>
-                {accordion ? (
-                    <h5>Expanded Accordion Heading</h5>
-                ) : (
-                    <h5>Closed Accordion Heading</h5>
-                )}
-                {accordion ? (
-                    <i class="fas fa-minus" onClick={handleAccordionClick}></i>
-                ) : (
-                    <i class="fas fa-plus" onClick={handleAccordionClick}></i>
-                )}
-            </section>
-            {accordion ? (
-                <section className="accordion--content">
-                    <div className="accordion--content__subheader"></div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Ut id purus at felis suscipit consequat id nec magna.
-                        Vestibulum ante ipsum primis in faucibus orci luctus et
-                        ultrices posuere cubilia curae; Nam purus odio, tempor
-                        sed justo non, tincidunt suscipit leo. Suspendisse et
-                        nibh nec neque iaculis molestie lacinia id nisl. Nullam
-                        nisl metus, semper ac erat ut, suscipit ullamcorper
-                        odio. Nunc et metus ut odio posuere ultricies in id
-                        erat. Quisque luctus posuere est ut facilisis. Morbi
-                        auctor, justo blandit vehicula fringilla, sem erat
-                        commodo nulla, at scelerisque tellus turpis eget risus.
-                        Donec et nulla in elit aliquet molestie. Suspendisse
-                        varius finibus aliquam. Donec laoreet interdum elit et
-                        malesuada. Nullam pharetra felis vitae lorem pulvinar,
-                        rutrum viverra urna aliquet.
-                    </p>
-                </section>
-            ) : null}
-        </div>
+            {MOCK_DATA.map((el, idx) => {
+                return (
+                    <div
+                        className={
+                            selectedAction ? `accordion--active` : `accordion`
+                        }
+                        key={idx}>
+                        <section
+                            className={
+                                selectedAction
+                                    ? `accordion--header__active`
+                                    : `accordion--header`
+                            }
+                        >
+                            {selectedAction ? (
+                                <h5>{el.header}</h5>
+                            ) : (
+                                <h5>{el.header}</h5>
+                            )}
+                            {selectedAction ? (
+                                <i
+                                    class="fas fa-minus"
+                                    onClick={handleAccordionClick}
+                                ></i>
+                            ) : (
+                                <i
+                                    class="fas fa-plus"
+                                    onClick={handleAccordionClick}
+                                ></i>
+                            )}
+                        </section>
+                        {selectedAction ? (
+                            <section className="accordion--content">
+                                <div className="accordion--content__subheader"></div>
+                                <p>{el.text}</p>
+                            </section>
+                        ) : null}
+                    </div>
+                )
+            })}
         </>
     )
 }
