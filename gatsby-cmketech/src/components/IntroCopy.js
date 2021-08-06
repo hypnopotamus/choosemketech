21b5e8a61733165cd75a48dcc196c87c10eb89a5
nvisiawwebster <wwebster@nvisia.com>
2021-08-06 15:26:35 -0500

    adjusted SCSS to simplify and improve readability

--- gatsby-cmketech/src/components/IntroCopy.js
+++ gatsby-cmketech/src/components/IntroCopy.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 const IntroCopy = ({heroModifier}) => {
     return (
         <div className="row">
-            <section className={heroModifier ? `intro intro-copy intro-copy--hero` : `intro intro intro-copy`}>
+            <section className={heroModifier ? `intro intro-copy--hero` : `intro-copy`}>
                 <h2>Choose Milwaukee Tech</h2>
                 <h3>Voted Best City for Startups in Tech</h3>
                 <p>Find your next job in tech in the Midwest’s hottest rising tech hub. Milwaukee is home to household names in tech, globally recognized water technology, exciting start-ups, and a vibrant tech community eager to explore the next emerging technology. <strong>Come see what’s brewing in Milwaukee.</strong></p>
