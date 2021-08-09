810a2a6b23c2023f01986e577350a515580d2c5b
nvisiawwebster <wwebster@nvisia.com>
2021-08-09 16:45:56 -0500

    grid template starting

--- gatsby-cmketech/src/components/MembershipTiers.js
+++ gatsby-cmketech/src/components/MembershipTiers.js
@@ -6,23 +6,34 @@ const MembershipTiers = ({}) => {
         <section className="memberships--tier container">
             <div className="memberships--perks">
                 <div>INCLUDES</div>
+                <p>Member Benefits</p>
             </div>
             <div className="memberships--feature">
                 <section className="memberships--feature__grid">
-                    <p>
-                        Premium <span>For enterprises tech companies</span>
-                    </p>
-                    <p>
-                        Premium <span>For enterprises tech companies</span>
-                    </p>
-                    <p>
-                        Premium <span>For enterprises tech companies</span>
-                    </p>
-                    <p>
-                        Premium <span>For enterprises tech companies</span>
-                    </p>
+                    <div>
+                        <h4>Premium</h4>
+                        <span>For enterprises tech companies</span>
+                    </div>
+                    <div>
+                        <h4>Enhanced</h4>
+                        <span>For mid to large tech employers</span>
+                    </div>
+                    <div>
+                        <h4>Base</h4>
+                        <span>For small tech employers</span>
+                    </div>
+                    <div>
+                        <h4>Premium</h4>
+                        <span>For startups + engaged community members</span>
+                    </div>
                 </section>
             </div>
+            <div class="membership--perks">NEWWWW</div>
+            <div class="membership--perks">NEWWWW</div>
+            <div class="membership--perks">NEWWWW</div>
+            <div class="membership--perks">NEWWWW</div>
+
+
         </section>
     )
 }
