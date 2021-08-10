81b6c865d1ac5b6e8eaf4e5a7a2d6ba9974c756e
nvisiawwebster <wwebster@nvisia.com>
2021-08-10 13:49:47 -0500

    backup added for grid format

--- gatsby-cmketech/src/components/MembershipTiers.js
+++ gatsby-cmketech/src/components/MembershipTiers.js
@@ -100,17 +100,26 @@ const MembershipTiers = ({}) => {
 export default MembershipTiers
 
 /*
-
-
-<section className="memberships--tier container">
+Back up 
+         <section className="memberships--tier container">
             <div className="memberships--perks">
                 <h4>INCLUDES</h4>
-                <p>Member Benefits</p>
+                <p className="memberships--perks__header">Member Benefits</p>
                 <label>Company Profile Listing</label>
                 <label>Link To Job Board</label>
                 <label>Tech Company Listing</label>
                 <label>Top Job Search Placement</label>
                 <label>Job Listings</label>
+                <p className="memberships--perks__header">Homepage Features</p>
+                <label>Profile Listing</label>
+                <label>Featured Jobs</label>
+                <label>Community Spotlight</label>
+                <p className="memberships--perks__header">
+                    Company Profile Features
+                </p>
+                <label>Social Media Links</label>
+                <label>Company Stories</label>
+                <label>Profile</label>
             </div>
             <div className="memberships--tiers">
                 <section className="memberships--feature__grid">
@@ -145,31 +154,44 @@ export default MembershipTiers
                 <span>Premium Placement</span>
                 <span>Limited Details</span>
                 <span>Name and Location Only</span>
+                <i class="fas fa-check"></i>
+                <i class="fas fa-check"></i>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
+                <span>12/month</span>
+                <span>7/month</span>
+                <span>2/month</span>
+                <span>Available for purchase</span>
+            </div>
+            <div class="memberships--features">
+                <span>4/year</span>
+                <span>2/year</span>
+                <span>1/year</span>
+                <i class="fas fa-times"></i>
+                <span>2/month</span>
+                <span>1/month</span>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
+                <span>2/month</span>
+                <span>1/month</span>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
+            </div>
+            <div class="memberships--features">
+                <i class="fas fa-check"></i>
+                <i class="fas fa-check"></i>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
+                <span>5 stories</span>
+                <span>3 stories</span>
+                <span>1 stories</span>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-check"></i>
+                <i class="fas fa-check"></i>
+                <i class="fas fa-times"></i>
+                <i class="fas fa-times"></i>
             </div>
         </section>
 
-
-
-
-         <div class="new-membership-grid-parent">
-                <div class="div1">
-                    <i class="fas fa-check"></i>
-                    <i class="fas fa-check"></i>
-           
-                </div>
-                <div class="div2">
-                    <i class="fas fa-check"></i>
-                </div>
-                <div class="div3">
-                    <i class="fas fa-check"></i>
-                </div>
-                <div class="div4">
-                    <i class="fas fa-check"></i>
-                </div>
-                <div class="div5">
-                    <i class="fas fa-check"></i>
-                </div>
-                <div class="div6"> x</div>
-                <div class="div6"> x</div>
-            </div>
+ 
         */
