a79569442bf641e60245e50d0e59fae13488440c
stephen <smahadeo@nvisia.com>
2021-08-11 19:57:51 -0500

    added fav icon

--- gatsby-cmketech/src/components/Layout.js
+++ gatsby-cmketech/src/components/Layout.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import favicon from '../assets/images/favicon.png'
+import Helmet from 'react-helmet'
 import Header from "./Header";
 import Hero from "./Hero";
-import IntroCopy from "./IntroCopy";
-import CardCopy from "./CardCopy";
 import JobCarousel from "./JobCarousel";
 import ImageGrid from "./ImageGrid";
 import Cards from "./Cards";
@@ -16,6 +16,7 @@ import ThreeColumn from "./ThreeColumn";
 const backdropClasses = " backdrop"
 
 const Layout = ({ children, bodyClass }) => {
+  
   const [backdropActive, setBackdropActive] = useState(false)
 
   const toggleBackdrop = (e, active) => {
@@ -25,6 +26,9 @@ const Layout = ({ children, bodyClass }) => {
 
   return (
     <div id={"GatsbyBody"} className={ bodyClass + " showing-menu-modal showing-modal" + (backdropActive ? backdropClasses : "") } >
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
 
       <Header toggleBackdrop={toggleBackdrop} />
 
