e066bc1389921b57a5da0010e778439429636619
Oliver Ramirez-Velazquez <oramirez@nvisia.com>
2021-08-18 15:18:19 -0500

    dropdowns!

--- gatsby-cmketech/src/components/FormInput.js
+++ gatsby-cmketech/src/components/FormInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
+import MultiSelect from "react-multi-select-component"
 
 const FormInput = ({
     name,
@@ -14,6 +15,7 @@ const FormInput = ({
     ...props
 }) => {
     const [passwordShown, setPasswordShown] = useState(false)
+    const [selected, setSelected] = useState([])
     const handlePasswordClick = (i) => {
         setPasswordShown(!passwordShown)
     }
@@ -32,7 +34,9 @@ const FormInput = ({
                             className={className}
                             style={error && { border: "solid 1px #B11030" }}
                         ></textarea>
-                        {error && <i className="fas fa-exclamation-triangle"></i>}
+                        {error && (
+                            <i className="fas fa-exclamation-triangle"></i>
+                        )}
                     </span>
                     {error && <p className="form-field--error"> {error}</p>}
                 </div>
@@ -52,7 +56,9 @@ const FormInput = ({
                             className={className}
                             style={error && { border: "solid 1px #B11030" }}
                         />
-                        {error && <i className="fas fa-exclamation-triangle"></i>}
+                        {error && (
+                            <i className="fas fa-exclamation-triangle"></i>
+                        )}
                     </span>
                     {error && <p className="form-field--error"> {error}</p>}
                 </div>
@@ -63,54 +69,92 @@ const FormInput = ({
                     <label htmlFor={name}>{label}</label>
                     <span className="form-field__input-container">
                         <span className="form-field__password-field">
-                        <input
-                            id={name}
-                            name={name}
-                            type={passwordShown ? 'text' : 'password'}
-                            placeholder={placeholder}
-                            onChange={onChange}
-                            value={value}
-                            className={className}
-                            style={error && { border: "solid 1px #B11030" }}
-                        />
-                        { passwordShown ? (
-                            <button onClick={()=>handlePasswordClick()} className="form-field__password-field__button form-field__password-field__show-button" type="button"><i className="fas fa-eye"></i></button>
-                        ) : (
-                            <button onClick={()=>handlePasswordClick()} className="form-field__password-field__button form-field__password-field__hide-button" type="button"><i className="fas fa-eye-slash"></i></button>                            
-                        )}                        
+                            <input
+                                id={name}
+                                name={name}
+                                type={passwordShown ? "text" : "password"}
+                                placeholder={placeholder}
+                                onChange={onChange}
+                                value={value}
+                                className={className}
+                                style={error && { border: "solid 1px #B11030" }}
+                            />
+                            {passwordShown ? (
+                                <button
+                                    onClick={() => handlePasswordClick()}
+                                    className="form-field__password-field__button form-field__password-field__show-button"
+                                    type="button"
+                                >
+                                    <i className="fas fa-eye"></i>
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => handlePasswordClick()}
+                                    className="form-field__password-field__button form-field__password-field__hide-button"
+                                    type="button"
+                                >
+                                    <i className="fas fa-eye-slash"></i>
+                                </button>
+                            )}
                         </span>
-                        
 
-                        {error && <i className="fas fa-exclamation-triangle"></i>}
+                        {error && (
+                            <i className="fas fa-exclamation-triangle"></i>
+                        )}
                     </span>
                     {error && <p className="form-field--error"> {error}</p>}
                 </div>
             )
         case "checkbox":
-            if(props.slider){
-                return(
+            if (props.slider) {
+                return (
                     <div className="form-field form-slider">
-                    <label htmlFor={name}>{label}</label>
-                    <span className="form-field__toggle">
-                        <input
-                            id={name}
-                            name={name}
-                            type={type}
-                            placeholder={placeholder}
-                            onChange={onChange}
-                            value={value}
-                            className={className}
-                            style={error && { border: "solid 1px #B11030" }}
-                        />
-                        <span className="slider-toggle"></span>
-                        {error && <i className="fas fa-exclamation-triangle"></i>}
-                    </span>
-                    {error && <p className="form-field--error"> {error}</p>}
-                </div>
+                        <label htmlFor={name}>{label}</label>
+                        <span className="form-field__toggle">
+                            <input
+                                id={name}
+                                name={name}
+                                type={type}
+                                placeholder={placeholder}
+                                onChange={onChange}
+                                value={value}
+                                className={className}
+                                style={error && { border: "solid 1px #B11030" }}
+                            />
+                            <span className="slider-toggle"></span>
+                            {error && (
+                                <i className="fas fa-exclamation-triangle"></i>
+                            )}
+                        </span>
+                        {error && <p className="form-field--error"> {error}</p>}
+                    </div>
                 )
-            }else {
+            } else {
                 return (
                     <div className="form-field checkbox">
+                        <label htmlFor={name}>{label}</label>
+                        <span className="form-field__input-container">
+                            <input
+                                id={name}
+                                name={name}
+                                type={type}
+                                placeholder={placeholder}
+                                onChange={onChange}
+                                value={value}
+                                className={className}
+                                style={error && { border: "solid 1px #B11030" }}
+                            />
+                            {error && (
+                                <i className="fas fa-exclamation-triangle"></i>
+                            )}
+                        </span>
+                        {error && <p className="form-field--error"> {error}</p>}
+                    </div>
+                )
+            }
+        case "radio":
+            return (
+                <div className="form-field radio">
                     <label htmlFor={name}>{label}</label>
                     <span className="form-field__input-container">
                         <input
@@ -123,10 +167,62 @@ const FormInput = ({
                             className={className}
                             style={error && { border: "solid 1px #B11030" }}
                         />
-                        {error && <i className="fas fa-exclamation-triangle"></i>}
+                        {error && (
+                            <i className="fas fa-exclamation-triangle"></i>
+                        )}
                     </span>
                     {error && <p className="form-field--error"> {error}</p>}
                 </div>
+            )
+        case "dropdown":
+            if (props.multiSelect) {
+                return (
+                    <div
+                        className={`form-field dropdown ${
+                            props.halfLength ? "half-length" : ""
+                        }`}
+                    >
+                        <label htmlFor={name}>{label}</label>
+                        <MultiSelect
+                            options={children}
+                            value={selected}
+                            onChange={setSelected}
+                            labelledBy="Select"
+                        />
+                    </div>
+                )
+            } else {
+                return (
+                    <div
+                        className={`form-field dropdown ${
+                            props.halfLength ? "half-length" : ""
+                        }`}
+                    >
+                        <label htmlFor={name}>{label}</label>
+                        <div className="select-container">
+                            <div className="select-container__select">
+                                <select
+                                    name={name}
+                                    id={name}
+                                    style={
+                                        error && { border: "solid 1px #B11030" }
+                                    }
+                                >
+                                    <option value="">Select option</option>
+                                    {children.map((option) => (
+                                        <option key={option.value}>
+                                            {" "}
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            {error && (
+                                <i className="fas fa-exclamation-triangle"></i>
+                            )}
+                        </div>
+                        {error && <p className="form-field--error"> {error}</p>}
+                    </div>
                 )
             }
     }
