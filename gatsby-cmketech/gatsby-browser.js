2aeb0b1242a3bdf3609810d9d1a54324c2bc09cf
stephen <smahadeo@nvisia.com>
2021-08-19 11:33:44 -0500

    added memberstack init function

--- gatsby-cmketech/gatsby-browser.js
+++ gatsby-cmketech/gatsby-browser.js
@@ -4,12 +4,17 @@ const memberStackScript = url => {
     const script = document.createElement("script")
     script.src = url
     script.setAttribute('data-memberstack-id','5fb7aa8ce9cc74cc2896c9ccc813d979')
-    script.async = true
     document.body.appendChild(script)
 }
 
 export const onClientEntry = () => {
     window.onload = () => {
         memberStackScript("https://api.memberstack.io/static/memberstack.js?custom")
+    }
+}
+
+export const onRouteUpdate = ({location, prevLocation}) => {
+    if(window.MemberStack) {
+        window.MemberStack.reload();
     }
 }
