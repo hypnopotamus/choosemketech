25d564537e80c11e31af25eb1b3a836c9c3b6851
stephen <smahadeo@nvisia.com>
2021-08-19 12:34:07 -0500

    added server side rendering file for loading memberstack to the head tag

---
+++ gatsby-cmketech/gatsby-ssr.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+    setHeadComponents([
+        <script type="text/javascript" src="https://api.memberstack.io/static/memberstack.js?custom" data-memberstack-id="5fb7aa8ce9cc74cc2896c9ccc813d979" />,
+   ]);
+}
