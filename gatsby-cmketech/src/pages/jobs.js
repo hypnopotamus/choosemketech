110428305d7caacbc1ca39f4991b2cd73b0ff506
stephen <smahadeo@nvisia.com>
2021-08-20 16:03:33 -0500

    fixed issues with jobs page

--- gatsby-cmketech/src/pages/jobs.js
+++ gatsby-cmketech/src/pages/jobs.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { graphql } from "gatsby"
 import Seo from "../components/Seo";
 import Layout from "../components/Layout";
 import Cards from "../components/Cards";
 
 const Jobs = () => {
-  
+
   return (
     <Layout>
-        
-        {/* <Seo title={title} description={excerpt} socialImage={featuredImage?.node} uri={uri} /> */}
-
+        <Seo title="Jobs Page" />
         <div className="container">
           <h1>Explore tech companies in Milwaukee</h1>
           <Cards />
@@ -19,22 +16,6 @@ const Jobs = () => {
   )
 
 }
-
-export const query = graphql`
-  query Jobs($id: String!, $nextPage: String, $previousPage: String) {
-    page: wpPage(id: { eq: $id }) {
-      ...PageContent
-    }
-    nextPage: wpPage(id: { eq: $nextPage }) {
-      title
-      uri
-    }
-    previousPage: wpPage(id: { eq: $previousPage }) {
-      title
-      uri
-    }
-  }
-`
   
 export default Jobs;
   
