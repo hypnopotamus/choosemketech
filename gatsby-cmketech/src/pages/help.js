ca4b6dfbd6c9f9eac4e88ba85cacd39da03caeaa
stephen <smahadeo@nvisia.com>
2021-08-20 08:19:06 -0500

    added help page template

---
+++ gatsby-cmketech/src/pages/help.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
+
+const Help = () => (
+    <Layout>
+        <Seo title="Help Page" />
+        <div className="container">
+          <h1>Help</h1>
+        </div>
+    </Layout>
+  )
+  
+export default Help;
+  
