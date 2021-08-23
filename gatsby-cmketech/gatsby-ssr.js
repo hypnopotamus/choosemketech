8171bace1bd67a58fb969d930bbe6325ca2231a9
stephen <smahadeo@nvisia.com>
2021-08-23 09:33:54 -0500

    added meta http equiv for if js is disabled

--- gatsby-cmketech/gatsby-ssr.js
+++ gatsby-cmketech/gatsby-ssr.js
@@ -3,5 +3,6 @@ import React from "react";
 export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
         <script type="text/javascript" src="https://api.memberstack.io/static/memberstack.js?custom" data-memberstack-id="5fb7aa8ce9cc74cc2896c9ccc813d979" />,
+        <noscript><meta httpEquiv="refresh" content="0; url=https://dev.choosemketech.org/no-script/" /></noscript>
    ]);
 }
