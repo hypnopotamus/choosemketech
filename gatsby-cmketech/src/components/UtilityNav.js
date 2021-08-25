d79735b4c5566cea1cfe8a703779fd5224dfc7f0
Oliver Ramirez-Velazquez <oramirez@nvisia.com>
2021-08-25 12:47:41 -0500

    company dropdown

--- gatsby-cmketech/src/components/UtilityNav.js
+++ gatsby-cmketech/src/components/UtilityNav.js
@@ -11,10 +11,16 @@ const UtilityNav = () => {
         e.preventDefault();
         setsubNavShow(!subNavShow);
     }
+    const handleMouseLeaveSubNav = () => {
+        setsubNavShow(false);
+    }
 
     if(window.MemberStack){
         window.MemberStack.onReady.then(function(member){
             let membershipType = member.membership.name;
+            if(membershipType != "Candidate") {
+                membershipType = "premium-plan"
+            }
             setProfileType(membershipType)
         });
         window.MemberStack.reload();
@@ -40,6 +46,7 @@ const UtilityNav = () => {
         return (
             <div className="utilityNav__member__subnav--container">
                 <ul 
+                    onMouseLeave={()=>handleMouseLeaveSubNav()}
                     role="menubar"
                     className={`nav__list nav__list--secondary ${subNavShow ? "showSubNav": ""}`}>
                     <div className="nav__user">
@@ -90,18 +97,95 @@ const UtilityNav = () => {
         return (
             <>
                 <li role="menuitem" className="utilityNav__item utilityNav__item--primary" data-ms-content="members">
-                    <a href="/company-profile"  className="utilityNav__link utilityNav__link--primary" data-ms-content="premium-plans">
+                    <a href="/company-profile"  className="utilityNav__link utilityNav__link--primary" >
                         <i className="fas fa-user"></i><span>Company Profile</span>
                     </a>
                 </li>
                 <li role="menuitem" className="utilityNav__item utilityNav__item--primary utilityNav__item--members" data-ms-content="members">
-                    <a href="#"  className="utilityNav__link utilityNav__link--primary" data-ms-content="premium-plans">
+                    <a href="#" onClick={(e) => handleDropdownClick(e)} className="utilityNav__link utilityNav__link--primary" >
                         <i className="fas fa-user-circle"></i><span data-ms-member="company-name"></span> <i className="fas fa-caret-down"></i>
                     </a>
+                    <CompanySubnav />
                 </li>
             </>
         )
     }
+    const CompanySubnav = () => {
+        return (
+            <div className="utilityNav__member__subnav--container">
+                <ul 
+                    onMouseLeave={()=>handleMouseLeaveSubNav()}
+                    role="menubar"
+                    className={`nav__list nav__list--secondary ${subNavShow ? "showSubNav": ""}`}>
+                    <div className="nav__user">
+                    <i className="fas fa-user-circle"></i><span data-ms-member="first-name"></span> 
+                    </div>
+                    
+                    <li className="nav__link-group__title">
+                        <h4>
+                            Company
+                        </h4>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="/member-profile">
+                            Company Profile
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="#">
+                            Job Postings
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="#">
+                            Candidate Pool
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="#">
+                            Listing + Credits
+                        </Link>
+                    </li>
+                    <li className="nav__link-group__title">
+                        <h4>
+                            Your Account
+                        </h4>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="/help">
+                            Settings
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="/faq">
+                            Notifications
+                        </Link>
+                    </li>
+                    <li className="nav__link-group__title">
+                        <h4>
+                            Other
+                        </h4>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="/help">
+                            Help
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="/faq">
+                            FAQs
+                        </Link>
+                    </li>
+                    <li className="nav__link">
+                        <Link to="#">
+                            Log Out
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
     return(
         <div className="utilityNav">
             <ul
@@ -121,7 +205,7 @@ const UtilityNav = () => {
                     ) : null
                 )}
                 { profileType === "Candidate" && <CandidateView /> }
-                { profileType === "premium-plans" && <CompanyView /> }
+                { profileType === "premium-plan" && <CompanyView /> }
             </ul>
         </div>
     )
