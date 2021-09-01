c1ebda7bf7cd75005bd1e8a8979144d5a4c334ef
stephen <smahadeo@nvisia.com>
2021-09-01 13:21:57 -0500

    updated about section

--- gatsby-cmketech/src/pages/about.js
+++ gatsby-cmketech/src/pages/about.js
@@ -6,53 +6,48 @@ import ImageCopy from "../components/ImageCopy";
 import SubHero from "../components/SubHero";
 
 const About = () => {
-   const data = useStaticQuery(graphql`
-     query AboutCFQuery {
-        wpPage(databaseId: {eq: 52}) {
-          About_CF {
-            subHeroTitle
-            subHeroSubTitle
-            subHeroContent
-            subHeroButtonUrl
-            subHeroButtonLabel
-            subHeroImage {
-              sourceUrl
-            }
-            aboutIntroCopySubheader
-            aboutIntroCopyHeadline
-            aboutIntroCopyContent
-            aboutImageCopyRepeater {
-              copy
-              image {
-                sourceUrl
-              }
-            }
+  const data = useStaticQuery(graphql`
+  query AboutCFQuery {
+    wpPage(databaseId: {eq: 52}) {
+      About_CF {
+        subHeroTitle
+        subHeroSubTitle
+        subHeroContent
+        subHeroButtonUrl
+        subHeroButtonLabel
+        subHeroImage {
+          sourceUrl
+        }
+        headerText
+        cardCollectionAbt {
+          title
+          content
+          buttonLabel
+          buttonUrl
+          featured
+          linkLabel
+          linkUrl
+          image {
+            sourceUrl
           }
         }
-     }
-    `)
+      }
+    }
+  }
+`)
 
-// let About = data.wpPage.About_CF;
-// console.log(About.aboutImageCopyRepeater)
+let About = data.wpPage.About_CF;
+console.log(About)
 
   return (
     <Layout>
         <Seo title="About" />
-          {/* <SubHero hero={About} /> */}
+          <SubHero hero={About} />
           <div className="container">
-          {/* <ImageCopy
-            header={false}
-            content={About.aboutImageCopyRepeater} 
-          /> */}
-
-          {/* <SubHero hero={WhyMKEData} />
-          <IntroCopy
-            headline={aboutQuery.wpPage.About_CF.aboutIntroCopyHeadline}
-            subheader={aboutQuery.wpPage.About_CF.aboutIntroCopySubheader}
-            copy={aboutQuery.wpPage.About_CF.aboutIntroCopyContent} />
-        <ImageCopy
-            header={false}
-            content={aboutQuery.wpPage.About_CF.aboutImageCopyRepeater} /> */}
+          <ImageCopy
+            cards={About.cardCollectionAbt}
+            title={About.headerText} 
+          />
         </div>
     </Layout>
   )
