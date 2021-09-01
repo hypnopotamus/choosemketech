19110d600fb6d6ad8f2ea660325513a3f4191ac5
stephen <smahadeo@nvisia.com>
2021-09-01 09:39:55 -0500

    added about page

---
+++ gatsby-cmketech/src/pages/about.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Seo from "../components/Seo";
+import Layout from "../components/Layout";
+import { graphql, useStaticQuery } from "gatsby";
+import IntroCopy from "../components/IntroCopy";
+import ImageCopy from "../components/ImageCopy";
+
+const About = () => {
+   const aboutQuery = useStaticQuery(graphql`
+     query AboutCFQuery {
+        wpPage(databaseId: {eq: 52}) {
+            About_CF {
+            aboutIntroCopyHeadline
+            aboutIntroCopySubheader
+            aboutIntroCopyContent
+            aboutImageCopyRepeater {
+                copy
+                image {
+                altText
+                link
+                }
+            }
+            }
+        }
+     }
+    `)
+  return (
+    <Layout>
+        <Seo title="About" />
+        <div className="container">
+          <h1>About</h1>
+          <IntroCopy
+            headline={aboutQuery.wpPage.About_CF.aboutIntroCopyHeadline}
+            subheader={aboutQuery.wpPage.About_CF.aboutIntroCopySubheader}
+            copy={aboutQuery.wpPage.About_CF.aboutIntroCopyContent} />
+        <ImageCopy
+            header={false}
+            content={aboutQuery.wpPage.About_CF.aboutImageCopyRepeater} />
+        </div>
+    </Layout>
+  )
+  
+}
+  
+export default About;
