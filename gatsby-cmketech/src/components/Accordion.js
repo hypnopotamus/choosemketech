6c5a9234fbde0e65ee80cfe9882621a108b39ded
stephen <smahadeo@nvisia.com>
2021-09-01 21:37:10 -0500

    added profile page

--- gatsby-cmketech/src/components/Accordion.js
+++ gatsby-cmketech/src/components/Accordion.js
@@ -1,53 +1,7 @@
 import React, { useState } from "react"
-import { Link } from "gatsby"
 
-const Accordion = ({ id }) => {
+const Accordion = ({items}) => {
     const [selectedAction, setSelectedAction] = useState(null)
-
-    const MOCK_DATA = [
-        {
-            id: 1,
-            tagName: `Lorem Ipsum`,
-            text: `Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit. Ut id purus at felis suscipit consequat id nec
-                    magna. Vestibulum ante ipsum primis in faucibus orci
-                    luctus et ultrices posuere cubilia curae; Nam purus
-                    odio, tempor sed justo non, tincidunt suscipit leo.
-                    Suspendisse et nibh nec neque iaculis molestie
-                    lacinia id nisl. Nullam nisl metus, semper ac erat
-                    ut, suscipit ullamcorper odio. Nunc et metus ut odio
-                    posuere ultricies in id erat. Quisque luctus posuere
-                    est ut facilisis. Morbi auctor, justo blandit
-                    vehicula fringilla, sem erat commodo nulla, at
-                    scelerisque tellus turpis eget risus. Donec et nulla
-                    in elit aliquet molestie. Suspendisse varius finibus
-                    aliquam. Donec laoreet interdum elit et malesuada.
-                    Nullam pharetra felis vitae lorem pulvinar, rutrum
-                    viverra urna aliquet.`,
-        },
-        {
-            id: 2,
-            tagName: `Lorem Ipsum 2 the sequel`,
-            text: ` Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit. Ut id purus at felis suscipit consequat id nec
-                    magna. Vestibulum ante ipsum primis in faucibus orci
-                    luctus et ultrices posuere cubilia curae; Nam purus
-                    odio, tempor sed justo non, tincidunt suscipit leo.
-                    Suspendisse et nibh nec neque iaculis molestie
-                  `,
-        },
-        {
-            id: 3,
-            tagName: `Stress test of how many characters can go here elwejwelkjwekjwkejlkwje`,
-            text: ` Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit. Ut id purus at felis suscipit consequat id nec
-                    magna. Vestibulum ante ipsum primis in faucibus orci
-                    luctus et ultrices posuere cubilia curae; Nam purus
-                    odio, tempor sed justo non, tincidunt suscipit leo.
-                    Suspendisse et nibh nec neque iaculis molestie
-                  `,
-        },
-    ]
 
     const handleAccordionClick = (i) => {
         if (selectedAction === i) {
@@ -57,8 +11,8 @@ const Accordion = ({ id }) => {
     }
 
     return (
-        <div className="row">
-            {MOCK_DATA.map((el, idx) => {
+        <div className="accordions--wrapper">
+            {items.map((el, idx) => {
                 return (
                     <div className="accordions" key={el.id}>
                         <div className={selectedAction == idx ? `accordion--active` : `accordion` } key={idx} onClick={() => handleAccordionClick(idx)}> 
--- gatsby-cmketech/src/pages/company-profile.js
+++ gatsby-cmketech/src/pages/company-profile.js
@@ -17,7 +17,6 @@ const CompanyProfile = () => {
     }
   }, [])
 
-  console.log(data)
   const featuredStory = [
     {
       title : "Embrace Diversity and Inclusion",
@@ -59,11 +58,11 @@ const CompanyProfile = () => {
           <div className="row">
             <div className="sidebar">
               <ul>
-                <li><i className="fas fa-map-marker-alt"></i> {data.address}</li>
-                <li><i className="fas fa-phone-alt"></i> {data['phone-number']}</li>
-                <li><i className="fab fa-linkedin-in"></i> {data.linkedin}</li>
-                <li><i className="fab fa-facebook-f"></i> {data.facebook}</li>
-                <li><i className="fab fa-twitter"></i> {data.twitter}</li>
+                <li className="address"><i className="fas fa-map-marker-alt"></i> <span>{data.address}</span></li>
+                <li className="phone"><i className="fas fa-phone-alt"></i> <span>{data['phone-number']}</span></li>
+                <li><i className="fab fa-linkedin-in"></i> <a href="" className="link">{data.linkedin}</a></li>
+                <li><i className="fab fa-facebook-f"></i> <a href="" className="link">{data.facebook}</a></li>
+                <li><i className="fab fa-twitter"></i> <a href="" className="link">{data.twitter}</a></li>
                 <li><Link to="/contact" className="button button--primary">Contact Us</Link></li>
               </ul>
             </div>
@@ -71,9 +70,9 @@ const CompanyProfile = () => {
               <div className="content__header">
                 <h1>Welcome {data['company-name']}!</h1>
                 <ul>
-                  <li><i className="fas fa-users"></i> Employees {data['total-of-employees']}</li>
-                  <li><i className="fas fa-briefcase"></i> Industry Type {data.industry}</li>
-                  <li><i className="fas fa-external-link-square-alt"></i> {data['company-url']}</li>
+                  <li><i className="fas fa-users"></i> <span>Employees {data['total-of-employees']}</span></li>
+                  <li><i className="fas fa-briefcase"></i> <span>Industry Type {data.industry}</span></li>
+                  <li><i className="fas fa-external-link-square-alt"></i> <a href={data['company-url']}>{data['company-url']}</a></li>
                 </ul>
               </div>
               <div className="content__description">
@@ -82,8 +81,10 @@ const CompanyProfile = () => {
               </div>
             </div>
           </div>
-          {/* <h2>Company Benefits + Perks</h2>
-          <Accordion /> */}
+          {/* <div className="accordions--wrapper">
+            <h2>Company Benefits + Perks</h2>
+            <Accordion items={data['perks-benefits']} />
+          </div> */}
           <ImageCopy cards={featuredStory} flip={true}/>
         </div>
     </Layout>
