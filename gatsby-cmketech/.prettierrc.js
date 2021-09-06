f3e4bffea927ba6ca67aa9fb83e28c6bd894688a
stephen <smahadeo@nvisia.com>
2021-09-06 02:19:44 -0500

    added prettier

--- gatsby-cmketech/.prettierrc.js
+++ gatsby-cmketech/.prettierrc.js
@@ -1,20 +1,21 @@
 module.exports = {
-  endOfLine: "lf",
-  semi: false,
-  singleQuote: false,
-  tabWidth: 4,
-  trailingComma: "es5",
-  overrides: [
-    {
-      // This file uses semicolons. It's needed here because `documentation`
-      // package (used to parse jsdoc and provide content for API reference pages)
-      // behaviour is inconsistent when not using semicolons after
-      // object declarations.
-      files: ["**/api-node-helpers-docs.js"],
-      options: {
-        bracketSpacing: false,
-        semi: true,
-      },
-    },
-  ],
+    endOfLine: "lf",
+    semi: false,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "es5",
+    printWidth: 1000,
+    overrides: [
+        {
+            // This file uses semicolons. It's needed here because `documentation`
+            // package (used to parse jsdoc and provide content for API reference pages)
+            // behaviour is inconsistent when not using semicolons after
+            // object declarations.
+            files: ["**/api-node-helpers-docs.js"],
+            options: {
+                bracketSpacing: false,
+                semi: true,
+            },
+        },
+    ],
 }
