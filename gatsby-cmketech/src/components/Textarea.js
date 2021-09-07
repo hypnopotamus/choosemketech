d243259e1dd57ef25efbf9c75b099862839ba082
stephen <smahadeo@nvisia.com>
2021-09-07 10:08:41 -0500

    useEffect for document widnow

--- gatsby-cmketech/src/components/Textarea.js
+++ gatsby-cmketech/src/components/Textarea.js
@@ -1,38 +1,38 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useEffect } from "react"
 
 const Textarea = ({ rows, cols, value, limit, maxlength, memberstack, id, name }) => {
-  if (typeof document !== `undefined`) {
+  useEffect(() => {
     if (name && name != null) {
       if (document.getElementById(name) != null) {
         value = document.getElementById(name).value
       }
     }
+  }, [])
 
-    let [content, textCount] = useState()
+  let [content, textCount] = useState()
 
-    const setTextareaCharacter = useCallback(
-      (text) => {
-        textCount(text.slice(0, limit))
-      },
-      [limit, textCount]
-    )
+  const setTextareaCharacter = useCallback(
+    (text) => {
+      textCount(text.slice(0, limit))
+    },
+    [limit, textCount]
+  )
 
-    let remainingContent
-    if (value) {
-      remainingContent = Math.abs(value.length - limit)
-    }
-
-    if (!remainingContent) {
-      remainingContent = limit
-    }
-
-    return (
-      <div className="textarea--container">
-        <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setTextareaCharacter(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
-        <p>{remainingContent} characters remain</p>
-      </div>
-    )
+  let remainingContent
+  if (value) {
+    remainingContent = Math.abs(value.length - limit)
   }
+
+  if (!remainingContent) {
+    remainingContent = limit
+  }
+
+  return (
+    <div className="textarea--container">
+      <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setTextareaCharacter(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
+      <p>{remainingContent} characters remain</p>
+    </div>
+  )
 }
 
 export default Textarea
