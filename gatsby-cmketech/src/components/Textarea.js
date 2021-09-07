b7aab1150f4f062a0c9fb95cb599dff703f110f6
stephen <smahadeo@nvisia.com>
2021-09-07 05:28:42 -0500

    checked for document

--- gatsby-cmketech/src/components/Textarea.js
+++ gatsby-cmketech/src/components/Textarea.js
@@ -1,36 +1,38 @@
 import React, { useState, useCallback } from "react"
 
 const Textarea = ({ rows, cols, value, limit, maxlength, memberstack, id, name }) => {
-  if (name && name != null) {
-    if (document.getElementById(name) != null) {
-      value = document.getElementById(name).value
+  if (typeof document !== `undefined`) {
+    if (name && name != null) {
+      if (document.getElementById(name) != null) {
+        value = document.getElementById(name).value
+      }
     }
+
+    let [content, textCount] = useState()
+
+    const setTextareaCharacter = useCallback(
+      (text) => {
+        textCount(text.slice(0, limit))
+      },
+      [limit, textCount]
+    )
+
+    let remainingContent
+    if (value) {
+      remainingContent = Math.abs(value.length - limit)
+    }
+
+    if (!remainingContent) {
+      remainingContent = limit
+    }
+
+    return (
+      <div className="textarea--container">
+        <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setTextareaCharacter(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
+        <p>{remainingContent} characters remain</p>
+      </div>
+    )
   }
-
-  let [content, textCount] = useState()
-
-  const setTextareaCharacter = useCallback(
-    (text) => {
-      textCount(text.slice(0, limit))
-    },
-    [limit, textCount]
-  )
-
-  let remainingContent
-  if (value) {
-    remainingContent = Math.abs(value.length - limit)
-  }
-
-  if (!remainingContent) {
-    remainingContent = limit
-  }
-
-  return (
-    <div className="textarea--container">
-      <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setTextareaCharacter(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
-      <p>{remainingContent} characters remain</p>
-    </div>
-  )
 }
 
 export default Textarea
