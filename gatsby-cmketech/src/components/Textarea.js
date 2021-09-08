5f51988d914859abe87f76555a2ae22595dcbab9
stephen <smahadeo@nvisia.com>
2021-09-08 10:34:55 -0500

    updated textarea

--- gatsby-cmketech/src/components/Textarea.js
+++ gatsby-cmketech/src/components/Textarea.js
@@ -1,32 +1,22 @@
 import React, { useState, useCallback, useEffect } from "react"
 
 const Textarea = ({ rows, cols, value, limit, maxlength, memberstack, id, name }) => {
+  const [count, setCharacterCount] = useState(700)
+  const [textCount, setTextCount] = useState([])
+
   useEffect(() => {
     if (name && name != null) {
       if (document.getElementById(name) != null) {
         value = document.getElementById(name).value
       }
+      setTextCount(Math.abs(value.length - limit))
     }
-
-    let [content, textCount] = useState()
-
-    const setTextareaCharacter = useCallback(
-      (text) => {
-        textCount(text.slice(0, limit))
-      },
-      [limit, textCount]
-    )
-
-    let remainingContent
-    if (value) {
-      remainingContent = Math.abs(value.length - limit)
-    }
-  })
+  }, [count])
 
   return (
     <div className="textarea--container">
-      <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setTextareaCharacter(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
-      <p>{remainingContent} characters remain</p>
+      <textarea id={id} name={name} rows={rows} cols={cols} onChange={(event) => setCharacterCount(event.target.value)} defaultValue={value} maxLength={maxlength} data-ms-member={memberstack} />
+      <p>{textCount} characters remain</p>
     </div>
   )
 }
