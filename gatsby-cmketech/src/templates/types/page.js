a5373a5641d6c6951cae77f2e3e884450aba66c7
stephen <smahadeo@nvisia.com>
2021-09-08 17:52:55 -0500

    updated page template

--- gatsby-cmketech/src/templates/types/page.js
+++ gatsby-cmketech/src/templates/types/page.js
@@ -4,15 +4,29 @@ import Layout from "../../components/Layout"
 import Seo from "../../components/Seo"
 import SubHero from "../../components/SubHero"
 import ImageGrid from "../../components/ImageGrid"
+import Breadcrumbs from "../../components/Breadcrumbs"
+import { useBreadcrumb } from "gatsby-plugin-breadcrumb"
 
 const page = ({ data }) => {
   const { page } = data
   const { title, content, featuredImage, excerpt, databaseId, uri } = page
+  const { crumbs } = useBreadcrumb({
+    location,
+    crumbLabel: title,
+  })
+
+  const heroData = {
+    subHeroTitle: title,
+    subHeroImage: {
+      sourceUrl: featuredImage.node.localFile.publicURL,
+    },
+  }
 
   return (
     <Layout bodyClass={`page-template-default page page-id-${databaseId} wp-embed-responsive singular missing-post-thumbnail has-no-pagination not-showing-comments footer-top-visible customize-support`}>
       <Seo title={title} description={excerpt} socialImage={featuredImage?.node} uri={uri} />
-      {/* <SubHero hero={data} /> */}
+      <Breadcrumbs crumbs={crumbs} />
+      <SubHero hero={heroData} />
       <div className="container">
         <h1 className="entry-title" dangerouslySetInnerHTML={{ __html: title }} />
         <div className="entry-content" dangerouslySetInnerHTML={{ __html: content }} />
