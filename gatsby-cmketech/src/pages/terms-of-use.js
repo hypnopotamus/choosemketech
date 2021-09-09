b0dac7c6a04d8f4e426d1f9b64359ac570f12567
stephen <smahadeo@nvisia.com>
2021-09-09 11:09:32 -0500

    updated terms

---
+++ gatsby-cmketech/src/pages/terms-of-use.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Seo from "../components/Seo"
+import Layout from "../components/Layout"
+import ImageCopy from "../components/ImageCopy"
+import SubHero from "../components/SubHero"
+import Breadcrumbs from "../components/Breadcrumbs"
+import { useBreadcrumb } from "gatsby-plugin-breadcrumb"
+
+const TermsOfServices = ({ location }) => {
+  const data = useStaticQuery(graphql`
+    query TermsOfServices {
+      wpPage(databaseId: { eq: 61 }) {
+        id
+        seo {
+          title
+          metaDesc
+          opengraphTitle
+          opengraphDescription
+          opengraphType
+          canonical
+        }
+        featuredImage {
+          node {
+            link
+          }
+        }
+        title
+        content
+      }
+    }
+  `)
+
+  let seoData = data.wpPage.seo
+  let featuredImage = data.wpPage.featuredImage.node.link
+
+  const { crumbs } = useBreadcrumb({
+    location,
+    crumbLabel: "Terms of Services",
+  })
+
+  const pageData = {
+    subHeroTitle: data.wpPage.title,
+    subHeroImage: {
+      sourceUrl: featuredImage,
+    },
+  }
+
+  return (
+    <Layout>
+      <Seo title={seoData.title} description={seoData.metaDesc} uri={seoData.canonical} socialImage={featuredImage} />
+
+      <Breadcrumbs crumbs={crumbs} />
+      <SubHero hero={pageData} />
+      <div className="container">
+        <div dangerouslySetInnerHTML={{ __html: data.wpPage.content }} />
+      </div>
+    </Layout>
+  )
+}
+
+export default TermsOfServices
