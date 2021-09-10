de97c6532cc39d2d450843be9893d5ac166f5d13
Oliver Ramirez-Velazquez <oramirez@nvisia.com>
2021-09-10 14:14:23 -0500

    updated 404 page

--- gatsby-cmketech/src/pages/404.js
+++ gatsby-cmketech/src/pages/404.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react"
 import Layout from "../components/Layout"
 
@@ -5,8 +6,10 @@ const NotFound = () => {
 
   return (
     <Layout>
-      <div className="container">
+      <div className="container text-center">
         <h1>Oops, that's a 404</h1>
+        <p>We can't find the page that you're looking for.</p>
+        <Link className="button button--primary" to="/"> Return To Home</Link>
       </div>
     </Layout>
   )
