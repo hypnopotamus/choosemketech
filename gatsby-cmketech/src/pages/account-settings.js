fa4287d3b1b9598710f58032092d2a701f61f7a0
stephen <smahadeo@nvisia.com>
2021-09-27 20:00:36 -0500

    updated secondary user

--- gatsby-cmketech/src/pages/account-settings.js
+++ gatsby-cmketech/src/pages/account-settings.js
@@ -47,13 +47,9 @@ const AccountSettings = ({ location }) => {
                 <h1>Account Settings</h1>
               </div>
               <h2>Set Secondary User</h2>
-              <div className="profiles__forms">
-                <div className="profiles__forms--cols">
-                  <FormInput name="primary_user" type="text" required label="Primary User" value={data.email} />
-                  <FormInput name="primary_userpassword" type="password" required label="Primary User Password" />
-                  <FormInput name="secondary_user" type="email" required label="Secondary User" placeholder="someone@domain.com" />
-                </div>
-              </div>
+              <FormInput name="primary_user" type="text" required label="Primary User" value={data.email} />
+              <FormInput name="primary_userpassword" type="password" required label="Primary User Password" />
+              <FormInput name="secondary_user" type="email" required label="Secondary User" placeholder="someone@domain.com" />
               <FormInput name="secondary_usernote" type="textarea" required label="Note" />
               <div className="form-controls">
                 <button type="submit" className="button button--primary">
